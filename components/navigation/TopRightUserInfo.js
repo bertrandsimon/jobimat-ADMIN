651243a4c83e1e179eebf6931b340e42eecf7fdd
bertrandsimon <trandber@hotmail.com>
2023-03-09 14:23:36 +0100

    button to post job from top bar in modal

--- components/navigation/TopRightUserInfo.js
+++ components/navigation/TopRightUserInfo.js
@@ -8,10 +8,16 @@ import IconButton from '@mui/material/IconButton';
 import SettingsIcon from '@mui/icons-material/Settings';
 import Badge from '@mui/material/Badge';
 import Tooltip from '@mui/material/Tooltip';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
 
 import { loggedStatus, loggedName, loggedToken } from '../../reducers/user';
 import { useDispatch, useSelector } from 'react-redux';
 
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+
+import JobPostForm from '../forms/JobPostForm';
 
 function TopRightUserInfo() {
   const dispatch = useDispatch();
@@ -21,6 +27,19 @@ function TopRightUserInfo() {
     window.location.href = 'http://localhost:3001/login'
     return null
   }
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
 
   const handleLogout = () => {
     dispatch(loggedStatus())
@@ -34,6 +53,8 @@ function TopRightUserInfo() {
 
       <Avatar alt="Remy Sharp" src="/images/1.jpg" />
       <span>Cynthia Duval</span>
+
+      <Button variant="contained" onClick={handleClickOpen}>Poster une annonce</Button>
 
       <Tooltip title="Messages">
         <Badge badgeContent={4} color="primary" >
@@ -52,6 +73,26 @@ function TopRightUserInfo() {
           <LogoutIcon />
         </IconButton>
       </Tooltip>
+
+
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        
+        <DialogContent>
+
+          
+          <JobPostForm/>
+        
+         
+
+        </DialogContent>
+  
+      </Dialog>
             
     </div>
 
